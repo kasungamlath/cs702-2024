52cd7d71cfdfdf51b1ff375863566bc3de94279f
kasun gamlath <kasun@kasuns-MacBook-Air.local>
2024-01-12 13:34:26 +0800

    week1 exercise1 solution

--- week-01/exercise/01_getting_started.py
+++ week-01/exercise/01_getting_started.py
@@ -4,6 +4,16 @@ Exercise 1.1: Getting Started
 
 
 if __name__ == "__main__":
+    while 1:
+        user_input = input("Guess what I ate today for lunch! (A: Pizza, B: Burger, C: Salad):")
+        # print("")
+        if user_input == "A":
+            print("You are right!")
+            break
+        else:
+            print("Try again !")
+        # print(input)
+
     # 1. Ask a question by printing "Guess what I ate today for lunch!" to the standard output.
 
     # 2. Provide options A, B, and C. (e.g., A: Pizza, B: Burger, C: Salad) by printing them to the standard output.
@@ -14,4 +24,4 @@ if __name__ == "__main__":
 
 
     # 4. Play the game with your partners!
-    pass
+    # pass
